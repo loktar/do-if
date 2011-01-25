b60ed7181792cfd6d616a410da9886d88262186f
gvanhove <gregg@slackersoft.net>
2011-01-25 08:27:48 -0800

    class << self

--- lib/do_if.rb
+++ lib/do_if.rb
@@ -3,45 +3,47 @@ require 'sha1'
 module DoIf
   YAML_FILE = "/tmp/do_if.yml"
   
-  def self.any_file_changed(file_glob, &block)
-    files = Dir.glob(file_glob)
+  class << self
+    def any_file_changed(file_glob, &block)
+      files = Dir.glob(file_glob)
     
-    FileUtils.touch(YAML_FILE) unless File.exists?(YAML_FILE)
-    cache = YAML.load_file(YAML_FILE) || {}
+      FileUtils.touch(YAML_FILE) unless File.exists?(YAML_FILE)
+      cache = YAML.load_file(YAML_FILE) || {}
 
-    return if files.empty? && !cache.has_key?(file_glob)
+      return if files.empty? && !cache.has_key?(file_glob)
     
-    file_hash = file_name_hash(files)
-    mtime = max_mtime(files)
+      file_hash = file_name_hash(files)
+      mtime = max_mtime(files)
     
-    if !cache.has_key?(file_glob) || (cache[file_glob] != {'file_names' => file_hash, 'max_mtime' => mtime})
-      update_cache_and_save cache, file_glob, file_hash, mtime
-      yield
-    end
-  end
-  
-  def self.any_file_changed_for_each_changed_file(file_glob, &block)
-    Dir.glob(file_glob).each do |file_path|
-      DoIf.any_file_changed(file_path) do
-        yield file_path
+      if !cache.has_key?(file_glob) || (cache[file_glob] != {'file_names' => file_hash, 'max_mtime' => mtime})
+        update_cache_and_save cache, file_glob, file_hash, mtime
+        yield
       end
     end
-  end
   
-  private
-  
-  def self.update_cache_and_save(cache, dir_name, file_hash, mtime)
-    cache[dir_name] = {"file_names" => file_hash, "max_mtime" => mtime}
-    File.open(YAML_FILE, 'w') do |f|
-      f.write(YAML.dump(cache))
+    def any_file_changed_for_each_changed_file(file_glob, &block)
+      Dir.glob(file_glob).each do |file_path|
+        DoIf.any_file_changed(file_path) do
+          yield file_path
+        end
+      end
     end
-  end
   
-  def self.file_name_hash(files)
-    SHA1.sha1(files.join("::")).to_s
-  end
+    private
   
-  def self.max_mtime(files)
-    files.collect {|f| File.mtime(f)}.max
+    def update_cache_and_save(cache, dir_name, file_hash, mtime)
+      cache[dir_name] = {"file_names" => file_hash, "max_mtime" => mtime}
+      File.open(YAML_FILE, 'w') do |f|
+        f.write(YAML.dump(cache))
+      end
+    end
+  
+    def file_name_hash(files)
+      SHA1.sha1(files.join("::")).to_s
+    end
+  
+    def max_mtime(files)
+      files.collect {|f| File.mtime(f)}.max
+    end
   end
 end
